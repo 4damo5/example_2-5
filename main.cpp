5d85c632f2f834838628f498c7d0d9a6c6e8f404
4damo5 <adamotsuka3@gmail.com>
2025-01-12 00:22:25 +0000

    first working product

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //=====[Libraries]=============================================================
 
-#include "mbed.h"
 #include "arm_book_lib.h"
+#include "mbed.h"
 
 //=====[Defines]===============================================================
 
@@ -9,225 +9,109 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
-DigitalIn enterButton(BUTTON1);
-DigitalIn gasDetector(D2);
-DigitalIn overTempDetector(D3);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
+DigitalIn driverPresent(D2);
+DigitalIn passengerPresent(D3);
+DigitalIn driverSeatbelt(D4);
+DigitalIn passengerSeatbelt(D5);
+DigitalIn ignitionButton(D6);
 
-DigitalOut alarmLed(LED1);
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
-
-UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+DigitalOut greenIndicator(LED1);
+DigitalOut alarmLed(LED3); //change to D7 and hook up alarm later
+DigitalOut blueIndicator(LED2);
 
 //=====[Declaration and initialization of public global variables]=============
 
-bool alarmState    = OFF;
-bool incorrectCode = false;
+bool alarmState = OFF;
 
-int numberOfIncorrectCodes = 0;
-int buttonBeingCompared    = 0;
-int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 };
-int buttonsPressed[NUMBER_OF_KEYS] = { 0, 0, 0, 0 };
+int ignitionCheck[NUMBER_OF_KEYS]   = { 0, 0, 0, 0 };
+int ignitionPermission[NUMBER_OF_KEYS] = { 1, 1, 1, 1};
 
 //=====[Declarations (prototypes) of public functions]=========================
 
 void inputsInit();
 void outputsInit();
 
-void alarmActivationUpdate();
-void alarmDeactivationUpdate();
-
-void uartTask();
-void availableCommands();
+bool alarmActivationUpdate();
+bool alarmDeactivationUpdate();
 bool areEqual();
 
 //=====[Main function, the program entry point after power on or reset]========
 
-int main()
-{
-    inputsInit();
-    outputsInit();
-    while (true) {
-        alarmActivationUpdate();
-        alarmDeactivationUpdate();
-        uartTask();
+int main() {
+  inputsInit();
+  outputsInit();
+  while (true) {
+    if (driverPresent) {
+      printf("Welcome to enhanced alarm system model 218-W24");
     }
+    while (driverPresent) {
+      if (alarmDeactivationUpdate()) {
+        greenIndicator = ON;
+      }
+
+      if (ignitionButton) {
+        if (alarmDeactivationUpdate()) {
+          printf("Engine started.");
+          greenIndicator = OFF;
+          blueIndicator = ON;
+
+          while (true) {
+          } // keeps the code stuck here
+        } else {
+          printf("Ignition inhibited");
+          printf("Reasons:"); // needs work to show reasons
+              alarmActivationUpdate();
+
+          while (true) {
+          } // keeps the code stuck here
+        }
+      }
+    }
+  }
 }
 
 //=====[Implementations of public functions]===================================
 
-void inputsInit()
-{
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
+void inputsInit() {
+  driverPresent.mode(PullDown);
+  passengerPresent.mode(PullDown);
+  driverSeatbelt.mode(PullDown);
+  passengerSeatbelt.mode(PullDown);
+  ignitionButton.mode(PullDown);
 }
 
-void outputsInit()
-{
-    alarmLed = OFF;
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
+void outputsInit() {
+  alarmLed = OFF;
+  blueIndicator = OFF;
+  greenIndicator = OFF;
 }
 
-void alarmActivationUpdate()
-{
-    if ( gasDetector || overTempDetector ) {
-        alarmState = ON;
-    }
-    alarmLed = alarmState;
+bool alarmActivationUpdate() {
+    ignitionCheck[0] = driverPresent;
+    ignitionCheck[1] = passengerPresent;
+    ignitionCheck[2] = driverSeatbelt;
+    ignitionCheck[3] = passengerSeatbelt;
+
+  if (ignitionButton && !areEqual()) {
+    alarmState = ON;
+  }
+
+  alarmLed = alarmState;
+  return true;
 }
 
-void alarmDeactivationUpdate()
-{
-    if ( numberOfIncorrectCodes < 5 ) {
-        if ( aButton && bButton && cButton && dButton && !enterButton ) {
-            incorrectCodeLed = OFF;
-        }
-        if ( enterButton && !incorrectCodeLed && alarmState ) {
-            buttonsPressed[0] = aButton;
-            buttonsPressed[1] = bButton;
-            buttonsPressed[2] = cButton;
-            buttonsPressed[3] = dButton;
-            if ( areEqual() ) {
-                alarmState = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }
-        }
-    } else {
-        systemBlockedLed = ON;
-    }
-}
+bool alarmDeactivationUpdate() {
+    ignitionCheck[0] = driverPresent;
+    ignitionCheck[1] = passengerPresent;
+    ignitionCheck[2] = driverSeatbelt;
+    ignitionCheck[3] = passengerSeatbelt;
 
-void uartTask()
-{
-    char receivedChar = '\0';
-    if( uartUsb.readable() ) {
-        uartUsb.read( &receivedChar, 1 );
-        switch (receivedChar) {
-        case '1':
-            if ( alarmState ) {
-                uartUsb.write( "The alarm is activated\r\n", 24);
-            } else {
-                uartUsb.write( "The alarm is not activated\r\n", 28);
-            }
-            break;
+  if (ignitionButton && areEqual()) {
+    alarmState = OFF;
+  }
 
-        case '2':
-            if ( gasDetector ) {
-                uartUsb.write( "Gas is being detected\r\n", 22);
-            } else {
-                uartUsb.write( "Gas is not being detected\r\n", 27);
-            }
-            break;
-
-        case '3':
-            if ( overTempDetector ) {
-                uartUsb.write( "Temperature is above the maximum level\r\n", 40);
-            } else {
-                uartUsb.write( "Temperature is below the maximum level\r\n", 40);
-            }
-            break;
-            
-        case '4':
-            uartUsb.write( "Please enter the code sequence.\r\n", 33 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 ); 
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for ", 41 );
-            uartUsb.write( "not pressed\r\n", 13 );
-            uartUsb.write( "For example, for 'A' = pressed, ", 32 );
-            uartUsb.write( "'B' = pressed, 'C' = not pressed, ", 34);
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            incorrectCode = false;
-
-            for ( buttonBeingCompared = 0; 
-                  buttonBeingCompared < NUMBER_OF_KEYS; 
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    if ( codeSequence[buttonBeingCompared] != 1 ) {
-                        incorrectCode = true;
-                    }
-                } else if ( receivedChar == '0' ) {
-                    if ( codeSequence[buttonBeingCompared] != 0 ) {
-                        incorrectCode = true;
-                    }
-                } else {
-                    incorrectCode = true;
-                }
-            }
-
-            if ( incorrectCode == false ) {
-                uartUsb.write( "\r\nThe code is correct\r\n\r\n", 25 );
-                alarmState = OFF;
-                incorrectCodeLed = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                uartUsb.write( "\r\nThe code is incorrect\r\n\r\n", 27 );
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }                
-            break;
-
-        case '5':
-            uartUsb.write( "Please enter new code sequence\r\n", 32 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 );
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for not ", 45 );
-            uartUsb.write( "pressed\r\n", 9 );
-            uartUsb.write( "For example, for 'A' = pressed, 'B' = pressed,", 46 );
-            uartUsb.write( " 'C' = not pressed,", 19 );
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            for ( buttonBeingCompared = 0; 
-                  buttonBeingCompared < NUMBER_OF_KEYS; 
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    codeSequence[buttonBeingCompared] = 1;
-                } else if ( receivedChar == '0' ) {
-                    codeSequence[buttonBeingCompared] = 0;
-                }
-            }
-
-            uartUsb.write( "\r\nNew code generated\r\n\r\n", 24 );
-            break;
-
-        default:
-            availableCommands();
-            break;
-
-        }
-    }
-}
-
-void availableCommands()
-{
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '1' to get the alarm state\r\n", 34 );
-    uartUsb.write( "Press '2' to get the gas detector state\r\n", 41 );
-    uartUsb.write( "Press '3' to get the over temperature detector state\r\n", 54 );
-    uartUsb.write( "Press '4' to enter the code sequence\r\n", 38 );
-    uartUsb.write( "Press '5' to enter a new code\r\n\r\n", 33 );
+  alarmLed = alarmState;
+  return false;
 }
 
 bool areEqual()
@@ -235,7 +119,7 @@ bool areEqual()
     int i;
 
     for (i = 0; i < NUMBER_OF_KEYS; i++) {
-        if (codeSequence[i] != buttonsPressed[i]) {
+        if (ignitionCheck[i] != ignitionPermission[i]) {
             return false;
         }
     }
